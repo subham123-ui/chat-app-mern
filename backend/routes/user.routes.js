ac9bf190d54ee34af10ef85205705b5ad6698968
subham123-ui <suvam.rockstar@gmail.com>
2024-03-14 23:02:26 +0530

    backend completed

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,7 +1,9 @@
 import express from "express";
+import protectRoute from "../middleware/protectRoute.js";
+import { getUserForSidebar } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.get("/", getUserForSidebar)
+router.get("/",protectRoute, getUserForSidebar)
 
 export default router;
