7651f497f603a69447cdc5660fd2aa4651f49371
subham123-ui <suvam.rockstar@gmail.com>
2024-03-21 22:33:45 +0530

    frontend complete

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -5,7 +5,7 @@ const LogoutButton = () => {
     <div className="mt-auto">
     <BiLogOut className="w-6 h-6 text-white cursor-pointer"/> 
     </div>
-  )
-}
+  );
+};
 
 export default LogoutButton
