4d244f310d3a984996ab65be8bd42559e9e493af
subham123-ui <suvam.rockstar@gmail.com>
2024-04-07 23:40:59 +0530

    func

--- frontend/src/components/hooks/useSendMessage.js
+++ frontend/src/components/hooks/useSendMessage.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import useConversation from "../zustand/useConversation";
+import useConversation from "../zustand/useConversation.js";
 import toast from "react-hot-toast";
 
 const useSendMessage = () => {
